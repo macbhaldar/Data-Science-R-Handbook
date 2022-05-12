c9e99a97cef4fc81180f70883e7e14fe6f84adff
Maksud Bhaldar <macbhaldar@gmail.com>
2022-05-12 22:13:41 +0530

    Create 3D-terrain-map-rendering.R

---
+++ Data-Visualization/rayshader/3D-terrain-map-rendering.R
@@ -0,0 +1,184 @@
+install.packages("rayshader")
+install.packages("rgdal")
+
+library(rayshader)
+
+# load a map with the raster package
+loadzip = tempfile() 
+download.file("https://tylermw.com/data/dem_01.tif.zip", loadzip)
+localtif = raster::raster(unzip(loadzip, "dem_01.tif"))
+unlink(loadzip)
+  
+# and convert it to a matrix:
+elmat = raster_to_matrix(localtif)
+
+# use another one of rayshader's built-in textures:
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  plot_map()
+
+# sphere_shade can shift the sun direction:
+elmat %>%
+  sphere_shade(sunangle = 45, texture = "desert") %>%
+  plot_map()
+
+# detect_water and add_water adds a water layer to the map:
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color = "desert") %>%
+  plot_map()
+
+# and we can add a raytraced layer from that sun direction as well:
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color = "desert") %>%
+  add_shadow(ray_shade(elmat), 0.5) %>%
+  plot_map()
+
+# and here we add an ambient occlusion shadow layer, which models 
+# lighting from atmospheric scattering:
+
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color = "desert") %>%
+  add_shadow(ray_shade(elmat), 0.5) %>%
+  add_shadow(ambient_shade(elmat), 0) %>%
+  plot_map()
+
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color = "desert") %>%
+  add_shadow(ray_shade(elmat, zscale = 3), 0.5) %>%
+  add_shadow(ambient_shade(elmat), 0) %>%
+  plot_3d(elmat, zscale = 10, fov = 0, theta = 135, zoom = 0.75, phi = 45, windowsize = c(1000, 800))
+Sys.sleep(0.2)
+render_snapshot()
+
+
+render_camera(fov = 0, theta = 60, zoom = 0.75, phi = 45)
+render_scalebar(limits=c(0, 5, 10),label_unit = "km",position = "W", y=50,
+                scale_length = c(0.33,1))
+render_compass(position = "E")
+render_snapshot(clear=TRUE)
+
+
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color = "lightblue") %>%
+  add_shadow(cloud_shade(elmat, zscale = 10, start_altitude = 500, end_altitude = 1000,), 0) %>%
+  plot_3d(elmat, zscale = 10, fov = 0, theta = 135, zoom = 0.75, phi = 45, windowsize = c(1000, 800),
+          background="darkred")
+render_camera(theta = 20, phi=40,zoom= 0.64, fov= 56 )
+
+render_clouds(elmat, zscale = 10, start_altitude = 800, end_altitude = 1000, attenuation_coef = 2, clear_clouds = T)
+render_snapshot(clear=TRUE)
+
+
+rgl::rgl.clear()
+
+
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color = "lightblue") %>%
+  add_shadow(cloud_shade(elmat,zscale = 10, start_altitude = 500, end_altitude = 700, 
+                         sun_altitude = 45, attenuation_coef = 2, offset_y = 300,
+                         cloud_cover = 0.55, frequency = 0.01, scale_y=3, fractal_levels = 32), 0) %>%
+  plot_3d(elmat, zscale = 10, fov = 0, theta = 135, zoom = 0.75, phi = 45, windowsize = c(1000, 800),
+          background="darkred")
+render_camera(theta = 125, phi=22,zoom= 0.47, fov= 60 )
+
+render_clouds(elmat, zscale = 10, start_altitude = 500, end_altitude = 700, 
+              sun_altitude = 45, attenuation_coef = 2, offset_y = 300,
+              cloud_cover = 0.55, frequency = 0.01, scale_y=3, fractal_levels = 32, clear_clouds = T)
+render_snapshot(clear=TRUE)
+
+
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color = "desert") %>%
+  plot_3d(elmat, zscale = 10, fov = 0, theta = 60, zoom = 0.75, phi = 45, windowsize = c(1000, 800))
+
+render_scalebar(limits=c(0, 5, 10),label_unit = "km",position = "W", y=50,
+                scale_length = c(0.33,1))
+
+render_compass(position = "E")
+Sys.sleep(0.2)
+render_highquality(samples=200, scale_text_size = 24,clear=TRUE)
+
+
+montshadow = ray_shade(montereybay, zscale = 50, lambert = FALSE)
+montamb = ambient_shade(montereybay, zscale = 50)
+montereybay %>%
+  sphere_shade(zscale = 10, texture = "imhof1") %>%
+  add_shadow(montshadow, 0.5) %>%
+  add_shadow(montamb, 0) %>%
+  plot_3d(montereybay, zscale = 50, fov = 0, theta = -45, phi = 45, 
+          windowsize = c(1000, 800), zoom = 0.75,
+          water = TRUE, waterdepth = 0, wateralpha = 0.5, watercolor = "lightblue",
+          waterlinecolor = "white", waterlinealpha = 0.5)
+Sys.sleep(0.2)
+render_snapshot(clear=TRUE)
+
+
+montereybay %>%
+  sphere_shade(zscale = 10, texture = "imhof1") %>%
+  plot_3d(montereybay, zscale = 50, fov = 70, theta = 270, phi = 30, 
+          windowsize = c(1000, 800), zoom = 0.6,  
+          water = TRUE, waterdepth = 0, wateralpha = 0.5, watercolor = "#233aa1",
+          waterlinecolor = "white", waterlinealpha = 0.5)
+Sys.sleep(0.2)
+render_highquality(lightdirection = c(-45,45), lightaltitude  = 30, clamp_value = 10, 
+                   samples = 256, camera_lookat= c(0,-50,0),
+                   ground_material = diffuse(color="grey50",checkercolor = "grey20", checkerperiod = 100),
+                   clear = TRUE)
+
+
+montereybay %>%
+  sphere_shade(zscale = 10, texture = "imhof1") %>%
+  plot_3d(montereybay, zscale = 50, fov = 70, theta = 270, phi = 30, 
+          windowsize = c(1000, 800), zoom = 0.6,  
+          water = TRUE, waterdepth = 0, wateralpha = 0.5, watercolor = "#233aa1",
+          waterlinecolor = "white", waterlinealpha = 0.5)
+Sys.sleep(0.2)
+render_highquality(lightdirection = c(-45,45), lightaltitude  = 30, clamp_value = 10, 
+                   samples = 256, camera_lookat= c(0,-50,0),
+                   ground_material = diffuse(color="grey50",checkercolor = "grey20", checkerperiod = 100),
+                   clear = TRUE)
+
+
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color = "desert") %>%
+  add_shadow(ray_shade(elmat, zscale = 3), 0.5) %>%
+  add_shadow(ambient_shade(elmat), 0) %>%
+  plot_3d(elmat, zscale = 10, fov = 30, theta = -225, phi = 25, windowsize = c(1000, 800), zoom = 0.3)
+Sys.sleep(0.2)
+render_depth(focallength = 800, clear = TRUE)
+
+library(ggplot2)
+ggdiamonds = ggplot(diamonds) +
+  stat_density_2d(aes(x = x, y = depth, fill = stat(nlevel)), 
+                  geom = "polygon", n = 200, bins = 50,contour = TRUE) +
+  facet_wrap(clarity~.) +
+  scale_fill_viridis_c(option = "A")
+
+par(mfrow = c(1, 2))
+
+plot_gg(ggdiamonds, width = 5, height = 5, raytrace = FALSE, preview = TRUE)
+plot_gg(ggdiamonds, width = 5, height = 5, multicore = TRUE, scale = 250, 
+        zoom = 0.7, theta = 10, phi = 30, windowsize = c(800, 800))
+Sys.sleep(0.2)
+render_snapshot(clear = TRUE)
+
+mtplot = ggplot(mtcars) + 
+  geom_point(aes(x = mpg, y = disp, color = cyl)) + 
+  scale_color_continuous(limits = c(0, 8))
+
+par(mfrow = c(1, 2))
+
+plot_gg(mtplot, width = 3.5, raytrace = FALSE, preview = TRUE)
+
+plot_gg(mtplot, width = 3.5, multicore = TRUE, windowsize = c(800, 800), 
+        zoom = 0.85, phi = 35, theta = 30, sunangle = 225, soliddepth = -100)
+Sys.sleep(0.2)
+render_snapshot(clear = TRUE)
